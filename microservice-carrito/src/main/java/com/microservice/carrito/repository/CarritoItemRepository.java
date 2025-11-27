63bf227a931052f5d81e89df56e96656eace6cdc
abraham abanto <ab.abanto@duocuc.cl>
2025-11-26 23:27:27 -0300

    Fix: Corregir metodo deleteByCarritoId en CarritoItemRepository usando Query personalizada

--- microservice-carrito/src/main/java/com/microservice/carrito/repository/CarritoItemRepository.java
+++ microservice-carrito/src/main/java/com/microservice/carrito/repository/CarritoItemRepository.java
@@ -2,12 +2,19 @@ package com.microservice.carrito.repository;
 
 import com.microservice.carrito.model.CarritoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface CarritoItemRepository extends JpaRepository<CarritoItem, Integer> {
-    List<CarritoItem> findByCarritoId(int carritoId);
-    void deleteByCarritoId(int carritoId);
+    @Query("SELECT ci FROM CarritoItem ci WHERE ci.carrito.id_carrito = :carritoId")
+    List<CarritoItem> findByCarritoId(@Param("carritoId") int carritoId);
+    
+    @Modifying
+    @Query("DELETE FROM CarritoItem ci WHERE ci.carrito.id_carrito = :carritoId")
+    void deleteByCarritoId(@Param("carritoId") int carritoId);
 }
 
